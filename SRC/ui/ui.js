e4e649f4fa60ed42f4fe69cb43529e11125f956e
lizethtriana <lvtriana@uniboyaca.edu.co>
2026-02-23 15:43:13 -0500

    avanceclase

---
+++ SRC/ui/ui.js
@@ -0,0 +1,21 @@
+export function showPokemon(pokemon){
+
+    //DATOS DEL POKEMON
+    document.getElementById(/*imagen*/).src=pokemon.sprite;
+    document.getElementById().textContent=capitalize(pokemon.name);
+    document.getElementById().textContent="#"+pokemon.id.toString().padStart(3,"0");
+
+    //TIPOS
+    const typesDiv=document.querySelector(".types");
+    typesDiv.innerHTML="";
+    pokemon.types.forEach(t =>{
+        const span=document.createElement("span");
+        span.classList.add("type",t);
+        span.textContent=capitalize(t);
+        typesDiv.appendChild(span);
+    });
+}   
+function capitalize (word){
+    return word.charAt(0).toUpperCase()+word.slice(1);
+    
+}
